1be064e3ca537e30235930a575108d6ac16be718
Dhruv Patel <61671874+dhruv-8601@users.noreply.github.com>
2021-03-15 10:53:44 -0400

    Add files via upload

---
+++ meetings/models.py
@@ -0,0 +1,25 @@
+from datetime import time
+
+from django.db import models
+
+# Please add a Model class called Room
+# To represent a meeting room
+# A room has a name, a floor number, and a room number
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floorNumber = models.IntegerField()
+    roomNumber = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}: room {self.roomNumber} on the {self.floorNumber}"
+
+# Create your models here.
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
